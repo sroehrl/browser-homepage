5239ac3a3d367d3291e4c4377530e39350ff2ecf
neoan <stefan.roehrl@gmail.com>
2023-08-18 08:26:26 -0400

    decloak darkmode

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,7 +4,9 @@
   import {page} from "$app/stores";
   import {base} from "$app/paths";
 </script>
-<DarkMode size="lg"/>
+<div class="p-3">
+    <DarkMode size="lg"/>
+</div>
 
 <div class="mt-5 w-4/5 mx-auto">
 <slot />
